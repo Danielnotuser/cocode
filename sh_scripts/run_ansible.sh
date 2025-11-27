092d1de36818f4cd63473a6b0a31b3b62ea98cb5
Danielnotuser <daniil.listikov@gmail.com>
2025-11-27 13:15:07 +0300

    sh ansible

--- sh_scripts/run_ansible.sh
+++ sh_scripts/run_ansible.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-INVENTORY="inventory.yml"
+INVENTORY="../ansible/inventory.yml"
 
 case "$1" in
     "deploy")
         echo "Deploying application..."
-        ansible-playbook -i $INVENTORY deploy.yml
+        ansible-playbook -i $INVENTORY ../ansible/deploy.yml
         ;;
     "stop")
         echo "Stopping servers..."
-        ansible-playbook -i $INVENTORY stop.yml
+        ansible-playbook -i $INVENTORY ../ansible/stop.yml
         ;;
 esac
